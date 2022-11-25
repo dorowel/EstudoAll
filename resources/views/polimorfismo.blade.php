1e8a94239e9292062b2b3270fa67f079dd1746d5
dorowel <dorowel@gmail.com>
2022-11-25 15:49:51 -0300

    polimorfismo

--- resources/views/polimorfismo.blade.php
+++ resources/views/polimorfismo.blade.php
@@ -1,3 +1,33 @@
 <?php
 
-echo 'cheguei';
+/**
+ *
+ */
+Class Animal {
+
+        public function Andar()
+        {
+
+        echo 'O animal andou';
+
+        }
+    public function Correr()
+    {
+        echo 'O animal Correu';
+
+    }
+
+    }
+
+   class Cavalo extends Animal{
+
+        public function Andar()
+            {
+              $this->Correr();
+            }
+
+   }
+
+    $animal = new Cavalo();
+    $animal->Andar ();
+
